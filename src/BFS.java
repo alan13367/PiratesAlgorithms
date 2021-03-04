38181e47f35bf543b29e22c63ab00b084c04352e
HRC12 <38540037+HRC12@users.noreply.github.com>
2021-03-04 16:25:41 +0100

    Update of the BFS algorithm, still not finished.

--- src/BFS.java
+++ src/BFS.java
@@ -5,26 +5,25 @@ public class BFS {
     /**
      * Searches through all the nodes with the BFS algorithm and displays the ones that are not safe.
      * @param graph The graph where the nodes are.
-     * @param node The ID of the node we are checking.
+     * @param nodeID The ID of the node we are checking.
      */
-    public LinkedList bfsAlgorithm(Graph graph, int node) {
+    public LinkedList bfsAlgorithm(Graph graph, int nodeID) {
         boolean[] visited = new boolean[graph.getNodes().size()];
         LinkedList<String> result = new LinkedList<>();
         LinkedList<Integer> queue = new LinkedList<>();
 
-        int index = graph.getIndex(node);
+        int index = graph.getIndex(nodeID);
         queue.add(index);
         visited[index] = true;
         while (queue.size() != 0) {
-            node = queue.poll();
+            index = queue.poll();
             if(!graph.getNodes().get(index).getSafe()) {
                 result.add(graph.getNodes().get(index).getName());
             }
-            for(int i = 0; i < graph.getNeighbours(node).size(); i++) {
-                if(!visited[graph.getIndex(graph.getNeighbours(node).get(i).getId())]) {
-                    int j = graph.getNeighbours(node).get(i).getId();
-                    queue.add(j);
-                    visited[j] = true;
+            for(Node neighbour: graph.getNeighbours(index)) {
+                if(!visited[graph.getIndex(neighbour.getId())]) {
+                    queue.add(neighbour.getId());
+                    visited[graph.getIndex(neighbour.getId())] = true;
                 }
             }
         }
