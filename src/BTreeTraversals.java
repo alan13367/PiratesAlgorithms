08c35baf9b195d333226ddc55217884a7bcd8ba2
Alan Beltran Pozo <alan.beltran@students.salle.url.edu>
2021-03-18 21:24:42 +0100

    PreOrder and InOrder implemented for Binary Trees Traversal

--- src/BTreeTraversals.java
+++ src/BTreeTraversals.java
@@ -1,12 +1,42 @@
 import java.util.ArrayList;
 
 public class BTreeTraversals {
-    public ArrayList<BTNode> preOrder(){
 
-        ArrayList<BTNode> result= new ArrayList<>();
+    /**
+     * This method will fill an ArrayList with the elements of the tree in preorder
+     * @param list Arraylist to store the nodes of the tree in preorder
+     * @param node the root from where the tree starts
+     */
+    public void preOrder(ArrayList<BTNode> list,BTNode node){
 
+        if(node == null){
+            return;
+        }
 
-        return result;
+        list.add(node);
+
+        preOrder(list,node.getlChild());
+
+        preOrder(list,node.getrChild());
     }
+
+    /**
+     * This method will fill an ArrayList with the elements of the tree in inorder
+     * @param list Arraylist to store the nodes of the tree in inorder
+     * @param node the root from where the tree starts
+     */
+    public void inOrder(ArrayList<BTNode> list,BTNode node){
+
+        if(node == null){
+            return;
+        }
+
+        inOrder(list,node.getlChild());
+
+        list.add(node);
+
+        inOrder(list,node.getrChild());
+    }
+
 
 }
