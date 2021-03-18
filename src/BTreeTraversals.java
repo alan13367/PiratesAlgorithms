d2ef5bba256054f02fb55ac5f8b894fefcc362c1
Alan Beltran Pozo <alan.beltran@students.salle.url.edu>
2021-03-18 17:26:22 +0100

    Binary Tree Traversals Initial

---
+++ src/BTreeTraversals.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class BTreeTraversals {
+    public ArrayList<BTNode> preOrder(){
+
+        ArrayList<BTNode> result= new ArrayList<>();
+
+
+        return result;
+    }
+
+}
